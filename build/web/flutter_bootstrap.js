26a1d3a3cf9e9b6ae72d5e3ab25abe97687d221f
LOCO <genspark_flutter_dev@genspark.ai>
2025-12-04 00:55:33 +0000

    fix: 完全リビルド - ログイン画面TOPリンク + デモログアウト修正

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "600842853"
+    serviceWorkerVersion: "2519720181"
   }
 });
